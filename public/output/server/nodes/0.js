afd7c8b53809ea1df5ac2dd50bcadd75976301a4
xPolar <polar@polar.blue>
2022-08-04 18:38:41 -0700

    lol2

--- public/output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/__layout.svelte.js';
-
-export { module };
-export const index = 0;
-export const file = '_app/immutable/pages/__layout.svelte-b48c01f5.js';
-export const imports = ["_app/immutable/pages/__layout.svelte-b48c01f5.js","_app/immutable/chunks/index-44f85c91.js"];
-export const stylesheets = ["_app/immutable/assets/__layout-740f831c.css"];
--- public/output/server/nodes/2.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/index.svelte.js';
-
-export { module };
-export const index = 2;
-export const file = '_app/immutable/pages/index.svelte-45a58ffa.js';
-export const imports = ["_app/immutable/pages/index.svelte-45a58ffa.js","_app/immutable/chunks/preload-helper-c28b9807.js","_app/immutable/chunks/index-44f85c91.js"];
-export const stylesheets = [];
